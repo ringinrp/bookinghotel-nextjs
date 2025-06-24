e49bc0b5844f9215287c1fdbaacdb960a2da9221
ringinrp <ringinrp21@gmail.com>
2025-06-24 18:00:15 +0700

    update

---
+++ app/about/page.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import HeaderSection from '@/components/header-section'
+import Image from 'next/image'
+import { IoEyeOutline, IoLocateOutline } from 'react-icons/io5'
+import {Metadata} from 'next'
+
+export const metadata:Metadata = {
+    title: "About",
+    description: "Who we are"
+}
+
+const AboutPage = () => {
+
+  return (
+    <div>
+        <HeaderSection title='About Us' subTitle='Lorem ipsum dolor sit amet.' />
+        <div className="max-w-screen-xl mx-auto py-20 px-4">
+            <div className="grid md:grid-cols-2 gap-8 ">
+                <Image src="/images/about-image.jpg" width={650} height={579} alt='about image' />
+                <div>
+                    <h1 className="text-5xl font-semibold text-gray-900 mb-4">Who We Are</h1>
+                    <p className="text-gray-700 py-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni possimus rem consectetur suscipit libero natus cumque qui ab. Eius, veniam.</p>
+                    <ul className="list-item space-y-6 pt-8">
+                        <li className='flex gap-5 '>
+                            <div className="flex-none mt-1">
+                                <IoEyeOutline className="size-7" />
+                            </div>
+                            <div className="flex-1">
+                                <h4 className="text-lg font-semibold mb-1">Vision : </h4>
+                                <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam voluptates maiores vero illum blanditiis?</p>
+                            </div>
+                        </li>
+                        <li className='flex gap-5 '>
+                            <div className="flex-none mt-1">
+                                <IoLocateOutline className="size-7" />
+                            </div>
+                            <div className="flex-1">
+                                <h4 className="text-lg font-semibold mb-1">Mission : </h4>
+                                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut sit corporis aliquid incidunt molestiae dolorem ea eius ullam tenetur. Incidunt.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutPage
--- components/header-section.tsx
+++ components/header-section.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image';
-import React from 'react'
 
-const HeaderSection = ({title, subtitle}:{
-    title:string;
-    subtitle:string;
+const HeaderSection = ({title, subTitle}:{
+    title: string;
+    subTitle: string;
 }) => {
+
   return (
-    <header className="relative h-[60px] text-white overflow-hidden">
+    <header className="relative h-60 text-white overflow-hidden">
         <div className='absolute inset-0'>
             <Image src="/images/hero.jpg" alt='img' fill className="object-cover object-center w-full h-full" />
-        <div className="absolute inset-0 bg-black opacity-50"></div>
+            <div className="absolute inset-0 bg-black opacity-50"></div>
         </div>
         <div className="relative flex flex-col justify-center items-center h-60 text-center pt-14">
             <h1 className="text-5xl font-bold leading-tight capitalize">{title}</h1>
-            <p className="text-xl text-gray-50">{subtitle}</p>
+            <p className="text-xl text-gray-300">{subTitle}</p>
         </div>
 
     </header>
