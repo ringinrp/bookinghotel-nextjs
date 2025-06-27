7378dfe791bdd3a81cd45856ce7485852aa21f63
ringinrp <ringinrp21@gmail.com>
2025-06-27 17:34:59 +0700

    update: auth

--- auth.ts
+++ auth.ts
@@ -6,5 +6,16 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   session: {strategy:"jwt"},
   pages:{
     signIn: "/signin",
+  },
+  callbacks : {
+    jwt({token, user}){
+      if(user) token.role =  user.role;
+      return token;
+    },
+    session({session, token}){
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      return session;
+    }
   }
 })
